38767cf01540f13b5f5511b1b3057f1a91fb2705
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-08-16 21:12:24 +0300

    Выполнение 7 задания ( Абстрактные классы и интерфейсы )

---
+++ Lesson7/Lesson7/BCode.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace Lesson_7
+{
+    /// <summary>
+    /// Класс, созданный для шифрования строки путём замена символа на символ с обратной позицией
+    /// </summary>
+    internal class BCode : ICoder
+    {
+        /// <summary>
+        /// Шифрование строки
+        /// </summary>
+        /// <param name="text">Строка для шифрования</param>
+        /// <param name="alfabet">Алфавит для шифрования строк</param>
+        /// <returns></returns>
+        public string Encoder(string text, char[] alfabet)
+        {
+            StringBuilder stringBuilder = new StringBuilder(text.Length);
+            var result = text.ToCharArray();
+            char charToAppend = ' ';
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].Equals(' '))
+                    stringBuilder.Append(' ');
+
+                for (int j = 0; j < alfabet.Length; j++)
+                {
+                    if (result[i] == alfabet[j])
+                    {
+                        charToAppend = alfabet[(alfabet.Length - 1) - j];
+                        stringBuilder.Append(charToAppend);
+                    }
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+
+        /// <summary>
+        /// Дешифрование строки
+        /// </summary>
+        /// <param name="text">Строка для дешифрования</param>
+        /// <param name="alfabet">Алфавит для дешифрования строк</param>
+        /// <returns></returns>
+        public string Decoder(string text, char[] alfabet)
+        {
+            return Encoder(text, alfabet);
+        }
+    }
+}
---
+++ Lesson7/Lesson7/ICoder.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson_7
+{
+    /// <summary>
+    /// Шифрование / дешифрование строк
+    /// </summary>
+    internal interface ICoder
+    {
+        /// <summary>
+        /// Шифрование строки
+        /// </summary>
+        /// <param name="text">Строка для шифрования</param>
+        /// <param name="alfabet">Алфавит для шифрования строк</param>
+        /// <returns></returns>
+        public string Encoder(string text, char[] alfabet);
+
+
+        /// <summary>
+        /// Шифрование строки
+        /// </summary>
+        /// <param name="text">Строка для дешифрования</param>
+        /// <param name="alfabet">Алфавит для дешифрования строк</param>
+        /// <returns></returns>
+        public string Decoder(string text, char[] alfabet);
+    }
+}
